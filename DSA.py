6cd7ff2893e3515b8c0d2e6fb3ac76e5896306b5
Prajjwal04jha <prajjwal04jha@gmail.com>
2026-05-28 11:22:48 +0530

    Initial Commit 007

---
+++ DSA.py
@@ -0,0 +1,34 @@
+class Solution:
+ 
+    def mergeSort(self, arr, l, r):
+        if len(arr)<=1:
+            return arr
+        mid= len(arr)//2
+        left_arr= arr[:mid]
+        right_arr= arr[mid:]
+        left_arr= self.mergeSort(left_arr, l ,r)
+        right_arr= self.mergeSort(right_arr,l , r)
+        return self.merge_arr(left_arr,right_arr)
+        
+        
+    
+        
+    def merge_arr(self, l,r):
+        i=0
+        j=0
+        empty=[]
+        while i<len(l) and j<len(r):
+            if l[i]<=r[j]:
+                empty.append(l[i])
+                i+=1
+            else:
+                empty.append(r[j])
+                j+=1
+                
+        empty.extend(l[i:])
+        empty.extend(r[j:])
+        return empty        
+    
+arr= [8,4,5,6,7,263,784,2]        
+sol = Solution()
+print(sol.mergeSort(arr, None, None))
